3aa0f1c53267b48250fdbf7009f43356fda1cc9b
Sami Anki <sami.anki1316@gmail.com>
2022-11-23 13:56:59 +0100

    new project

--- alpine/src/components/BottomBar/BottomBar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BottomBar = () => {
-  return <div>BottomBar</div>;
-};
-
-export default BottomBar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Home} from './pages';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
