c39925ed62e3d100511ce1f099a5b3e4eea45870
Sami Anki <sami.anki1316@gmail.com>
2022-11-24 15:40:39 +0100

    feat: add IconInfo

--- src/components/TripDescription/TripDescription.tsx
+++ src/components/TripDescription/TripDescription.tsx
@@ -1,6 +1,11 @@
 import styles from "./TripDescription.module.scss";
 import activeHeart from "assets/icons/heart-active.png";
 import inactiveHeart from "assets/icons/heart-not-active.png";
+import { ReactComponent as TimeSvg } from "assets/vectors/time.svg";
+import { ReactComponent as SpeedSvg } from "assets/vectors/speed.svg";
+import { ReactComponent as SpeedometerSvg } from "assets/vectors/speedometer.svg";
+import { ReactComponent as EuroSvg } from "assets/vectors/euro.svg";
+import IconInfo from "./IconInfo/IconInfo";
 
 type Props = {
   title: string;
@@ -21,6 +26,25 @@ const TripDescription = ({
     <div className={styles.container}>
       <h2>{title}</h2>
       <p className={styles.description}>{description}</p>
+
+      <div className={styles.tripInfos}>
+        <IconInfo
+          title="Average Speed"
+          value="75"
+          metric="Km/H"
+          icon={SpeedometerSvg}
+        />
+
+        <IconInfo
+          title="Max. Acceleration"
+          value="103"
+          metric="Km/H"
+          icon={SpeedSvg}
+        />
+
+        <IconInfo title="Price" value="13,4" metric="€" icon={EuroSvg} />
+        <IconInfo title="Duration" value="44" metric="min" icon={TimeSvg} />
+      </div>
 
       <div className={styles.btnContainer}>
         <div className={styles.btn}>
